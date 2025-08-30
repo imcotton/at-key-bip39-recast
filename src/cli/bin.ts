4491ee8926b47fb673080766280014e2d0b15fd0
Cotton Hou <himcotton@gmail.com>
2025-08-31 03:43:36 +0800

    refine error output during cli calling

--- src/cli/bin.ts
+++ src/cli/bin.ts
@@ -1,19 +1,33 @@
 #!/usr/bin/env node
 
-import { argv, stdout } from 'node:process';
+import { argv, stdout, stderr } from 'node:process';
+import { styleText } from 'node:util';
 import { main } from './main.ts';
 import { parse } from './parse.ts';
 
 
 
 
-main(parse(argv.slice(2))).then(function (data: string | Uint8Array) {
 
-    if (typeof data === 'string') {
-        return console.log(data);
+main(parse(argv.slice(2))).then(function (result) {
+
+    if (typeof result === 'string') {
+        return console.log(result);
     }
 
-    stdout.write(data);
+    if (result instanceof Uint8Array) {
+        return stdout.write(result);
+    }
+
+    const { note, err: { message } } = result;
+
+    console.error(styleText(
+        [ 'red', 'bold', 'underline' ],
+        message,
+        { stream: stderr },
+    ));
+
+    console.log(note);
 
 });
 

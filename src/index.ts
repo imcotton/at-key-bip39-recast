58bb4f5cba906acbf892bf246e083b512b7b5145
Cotton Hou <himcotton@gmail.com>
2025-09-04 06:21:39 +0800

    exports of rgba

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export { from_mnemonic, to_mnemonic, verify_mnemonic } from './bip39.ts';
 
 export { type Sentence, assert_sentence } from './utils.ts';
 
+export { type RGBA, mnemonic_to_rgba, rgba_to_mnemonic } from './rgba.ts';
+

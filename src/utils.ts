55c6d6c64871de35f6f1a92cd952a56f6fae93a7
Cotton Hou <himcotton@gmail.com>
2025-09-03 21:49:29 +0800

    it_to_rgba

--- src/utils.ts
+++ src/utils.ts
@@ -220,6 +220,24 @@ export function parse_int (base: number) {
 
 
 
+export function it_to_rgba ([ r, g, b, a ]: Iterable<number>) {
+
+    if (   r != null
+        && g != null
+        && b != null
+        && a != null
+    ) {
+        return [ r, g, b, a ] as const;
+    }
+
+    throw new Error('invalid 32-byte Iterable');
+
+}
+
+
+
+
+
 export function search_value <T> (arr: ReadonlyArray<T>) {
 
     return function * (indexes: Iterable<number>) {

60c77955e2efdd999b8125b571c8085c3094520b
Cotton Hou <himcotton@gmail.com>
2025-10-12 09:51:46 +0800

    test mnemonic_to_seed

--- spec/bip39.test.ts
+++ spec/bip39.test.ts
@@ -6,8 +6,43 @@ import {
     to_mnemonic,
     from_mnemonic,
     verify_mnemonic,
+    mnemonic_to_seed,
 
 } from '#src/bip39.ts';
+
+import * as u from '#src/utils.ts';
+
+import vectors from '#fixtures/vectors.json' with { type: 'json' };
+
+
+
+
+
+describe('mnemonic_to_seed', function () {
+
+    const sample = vectors.english.map(arr => arr.slice(1, 3));
+
+    it('passed off fixtures', async function () {
+
+        for (const [ phrase, hex ] of sample) {
+
+            const sentence = phrase?.split(' ');
+
+            u.assert_sentence(sentence);
+
+            const res = await mnemonic_to_seed(sentence, 'TREZOR');
+
+            ast.assertStrictEquals(
+                u.encode_hex(res),
+                hex,
+                sentence.slice(0, 5).join(' '),
+            );
+
+        }
+
+    });
+
+});
 
 
 

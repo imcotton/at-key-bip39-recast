e772256b76edf9326427b112b2baf210d0bf81a5
Cotton Hou <himcotton@gmail.com>
2025-10-12 09:52:35 +0800

    export mnemonic_to_seed

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
-export { from_mnemonic, to_mnemonic, verify_mnemonic } from './bip39.ts';
+export {
+    from_mnemonic, to_mnemonic, verify_mnemonic, mnemonic_to_seed,
+} from './bip39.ts';
 
 export { type Sentence, assert_sentence } from './utils.ts';
 

abddb7b209683dbbd0be384bd09f6e778f24b566
Cotton Hou <himcotton@gmail.com>
2025-11-02 10:07:30 +0800

    test reveal/collect { hash }

--- spec/cli/reveal.test.ts
+++ spec/cli/reveal.test.ts
@@ -5,9 +5,32 @@ import * as u from '#src/utils.ts';
 
 import {
 
+    collect,
     reversed_sri,
 
 } from '#src/cli/reveal.ts';
+
+
+
+
+
+describe('collect', function () {
+
+    it('collects hash', async function () {
+
+        const encode = async (str: string) => new TextEncoder().encode(str);
+
+        const buf = crypto.getRandomValues(new Uint8Array(16));
+        const b64 = u.encode_base64(buf);
+        const hash = reversed_sri(b64);
+
+        const [ res ] = await Array.fromAsync(collect({ hash }, encode));
+
+        ast.assertEquals(res, buf);
+
+    });
+
+});
 
 
 

f7a9eb88f9fa666fd0bf66be0035e20cc2f3b385
Cotton Hou <himcotton@gmail.com>
2026-05-07 00:59:33 +0800

    from_mnemonic_with_checksum

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export {
     from_mnemonic, to_mnemonic, verify_mnemonic, mnemonic_to_seed,
+    from_mnemonic_with_checksum,
 } from './bip39.ts';
 
 export { type Sentence, assert_sentence } from './utils.ts';

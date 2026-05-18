326b8179562e428e8630ba207f5200724c10a257
Cotton Hou <himcotton@gmail.com>
2026-05-18 17:50:44 +0800

    refine_sentence

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export {
     from_mnemonic, to_mnemonic, verify_mnemonic, mnemonic_to_seed,
-    from_mnemonic_with_checksum,
+    from_mnemonic_with_checksum, refine_sentence,
 } from './bip39.ts';
 
 export { type Sentence, assert_sentence } from './utils.ts';
